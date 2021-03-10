755da23e0b712c1628f4768c3c9cfb714d976437
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-03-10 14:43:38 +0100

    Create Spacing Rule

--- SkladoveKarty/GlobalSuppressions.cs
+++ SkladoveKarty/GlobalSuppressions.cs
@@ -5,6 +5,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1000:Keywords should be spaced correctly", Justification = "Doesn't work with C# 9.0 new().")]
 [assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:Braces should not be omitted", Justification = "It's clearer sometimes.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "I don't need it.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1601:Partial elements should be documented", Justification = "I don't need it.")]
