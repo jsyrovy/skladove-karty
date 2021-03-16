ab394234a67c8101864c1fef5816716d4c3f14c0
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-03-16 17:52:58 +0100

    Create IDatabaseContext

--- SkladoveKarty/Models/DatabaseContext.cs
+++ SkladoveKarty/Models/DatabaseContext.cs
@@ -2,28 +2,28 @@
 {
     using Microsoft.EntityFrameworkCore;
 
-    public class DatabaseContext : DbContext
+    public class DatabaseContext : DbContext, IDatabaseContext
     {
         private const string DefaultValueSql = "DATETIME()";
         private const string DatabaseName = "data.sqlite";
 
         private ModelBuilder modelBuilder;
 
-        public DbSet<StorageCard> StorageCards { get; set; }
+        public virtual DbSet<StorageCard> StorageCards { get; set; }
 
-        public DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
 
-        public DbSet<Store> Stores { get; set; }
+        public virtual DbSet<Store> Stores { get; set; }
 
-        public DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Account> Accounts { get; set; }
 
-        public DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
 
-        public DbSet<StorageCardSupplier> StorageCardSuppliers { get; set; }
+        public virtual DbSet<StorageCardSupplier> StorageCardSuppliers { get; set; }
 
-        public DbSet<Item> Items { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
 
-        public DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite($"Data Source={DatabaseName}");
---
+++ SkladoveKarty/Models/IDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿namespace SkladoveKarty.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+    public interface IDatabaseContext
+    {
+        DbSet<Account> Accounts { get; set; }
+
+        DbSet<Category> Categories { get; set; }
+
+        DbSet<Customer> Customers { get; set; }
+
+        DbSet<Item> Items { get; set; }
+
+        DbSet<StorageCard> StorageCards { get; set; }
+
+        DbSet<StorageCardSupplier> StorageCardSuppliers { get; set; }
+
+        DbSet<Store> Stores { get; set; }
+
+        DbSet<Supplier> Suppliers { get; set; }
+
+        EntityEntry Add(object entity);
+
+        int SaveChanges();
+    }
+}
