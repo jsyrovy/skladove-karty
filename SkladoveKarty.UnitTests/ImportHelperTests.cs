4f7e3c5b24ac5e95551b8fc1973ae35c8eb41d06
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-04-04 18:05:29 +0200

    Create tests

--- SkladoveKarty.UnitTests/ImportHelperTests.cs
+++ SkladoveKarty.UnitTests/ImportHelperTests.cs
@@ -4,6 +4,8 @@
     using System.Collections.Generic;
     using System.Linq;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.ChangeTracking;
+    using Moq;
     using NUnit.Framework;
     using SkladoveKarty.Models;
     using SkladoveKarty.ViewModels.Helpers;
@@ -26,6 +28,95 @@
         public void TearDown()
         {
             this.databaseContext.Database.EnsureDeleted();
+        }
+
+        [Test]
+        public void ImportHelper_DatabaseContextIsNull_ThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ImportHelper(null));
+        }
+
+        [Test]
+        public void ImportItems_WhenCalled_ImportItems()
+        {
+            var importItem1 = new ImportExportItem
+            {
+                StorageCardName = "storageCard",
+                AccountName = "account",
+                CategoryName = "category",
+                StoreName = "store",
+                ItemDateTime = DateTime.Now,
+                ItemName = "item1",
+                ItemMovement = 1,
+                ItemQty = 2,
+                ItemPrice = 3,
+                ItemInvoice = "invoice1",
+                ItemCustomerName = null,
+            };
+            var importItem2 = new ImportExportItem
+            {
+                StorageCardName = "storageCard",
+                AccountName = "account",
+                CategoryName = "category",
+                StoreName = "store",
+                ItemDateTime = DateTime.Now,
+                ItemName = "item2",
+                ItemMovement = -1,
+                ItemQty = 5,
+                ItemPrice = 10,
+                ItemInvoice = "invoice2",
+                ItemCustomerName = "customer",
+            };
+            var importItems = new List<ImportExportItem>
+            {
+                importItem1,
+                importItem2,
+            };
+
+            var importHelper = new ImportHelper(this.databaseHelper);
+            importHelper.ImportItems(importItems);
+            this.databaseContext.SaveChanges();
+            var storageCard = this.databaseContext.StorageCards.Single();
+
+            Assert.That(storageCard.Name, Is.EqualTo("storageCard"));
+            Assert.That(storageCard.Account.Name, Is.EqualTo("account"));
+            Assert.That(storageCard.Category.Name, Is.EqualTo("category"));
+            Assert.That(storageCard.Store.Name, Is.EqualTo("store"));
+            Assert.That(storageCard.Items.Count, Is.EqualTo(2));
+            Assert.That(storageCard.Items[0].DateTime, Is.EqualTo(importItem1.ItemDateTime));
+            Assert.That(storageCard.Items[0].Name, Is.EqualTo(importItem1.ItemName));
+            Assert.That(storageCard.Items[0].Movement, Is.EqualTo(importItem1.ItemMovement));
+            Assert.That(storageCard.Items[0].Qty, Is.EqualTo(importItem1.ItemQty));
+            Assert.That(storageCard.Items[0].Price, Is.EqualTo(importItem1.ItemPrice));
+            Assert.That(storageCard.Items[0].Invoice, Is.EqualTo(importItem1.ItemInvoice));
+            Assert.That(storageCard.Items[0].Customer?.Name, Is.EqualTo(importItem1.ItemCustomerName));
+            Assert.That(storageCard.Items[1].DateTime, Is.EqualTo(importItem2.ItemDateTime));
+            Assert.That(storageCard.Items[1].Name, Is.EqualTo(importItem2.ItemName));
+            Assert.That(storageCard.Items[1].Movement, Is.EqualTo(importItem2.ItemMovement));
+            Assert.That(storageCard.Items[1].Qty, Is.EqualTo(importItem2.ItemQty));
+            Assert.That(storageCard.Items[1].Price, Is.EqualTo(importItem2.ItemPrice));
+            Assert.That(storageCard.Items[1].Invoice, Is.EqualTo(importItem2.ItemInvoice));
+            Assert.That(storageCard.Items[1].Customer?.Name, Is.EqualTo(importItem2.ItemCustomerName));
+        }
+
+        [Test]
+        public void ImportItems_ArgumentIsNull_ThrowException()
+        {
+            var databaseContext = new Mock<IDatabaseContext>();
+            databaseContext.Setup(d => d.ChangeTracker).Returns<ChangeTracker>(null);
+            databaseContext.Setup(d => d.Accounts).Throws<Exception>();
+            var databaseHelper = new DatabaseHelper(databaseContext.Object);
+            var importHelper = new ImportHelper(databaseHelper);
+
+            Assert.Throws<Exception>(() => importHelper.ImportItems(new List<ImportExportItem> { new() }));
+        }
+
+        [Test]
+        public void ImportItems_UnexpectedError_ThrowException()
+        {
+            var importHelper = new ImportHelper(this.databaseHelper);
+
+            Assert.Throws<ArgumentNullException>(() => importHelper.ImportItems(null));
         }
 
         [Test]
@@ -76,6 +167,14 @@
             Assert.That(storageCardSuppliers[2].StorageCard.Name, Is.EqualTo("storageCard2"));
             Assert.That(storageCardSuppliers[2].Supplier, Is.Not.Null);
             Assert.That(storageCardSuppliers[2].Supplier.Name, Is.EqualTo("supplier2"));
+        }
+
+        [Test]
+        public void ImportSuppliers_ArgumentIsNull_ThrowException()
+        {
+            var importHelper = new ImportHelper(this.databaseHelper);
+
+            Assert.Throws<ArgumentNullException>(() => importHelper.ImportSuppliers(null));
         }
 
         [Test]
