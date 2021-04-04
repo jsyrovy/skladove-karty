d8f4b13d81de69fadab592d77a0e69df440e4f63
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-04-04 14:20:01 +0200

    Create arguments check

--- SkladoveKarty/ViewModels/Helpers/HtmlHelper.cs
+++ SkladoveKarty/ViewModels/Helpers/HtmlHelper.cs
@@ -1,12 +1,15 @@
 ﻿namespace SkladoveKarty.ViewModels.Helpers
 {
-    using System.Collections.Generic;
+    using System;
     using Stubble.Core.Builders;
 
     public static class HtmlHelper
     {
         public static string Generate(string template, object model)
         {
+            if (template == null) throw new ArgumentNullException(nameof(template));
+            if (model == null) throw new ArgumentNullException(nameof(model));
+
             var stubble = new StubbleBuilder().Build();
 
             return stubble.Render(template, model);
