07ca4588e553807f9ca33a0c9476726485ef9fb6
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-04-11 22:45:44 +0200

    Create setting helper

---
+++ SkladoveKarty/ViewModels/Helpers/SettingHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace SkladoveKarty.ViewModels.Helpers
+{
+    using System;
+    using System.Linq;
+    using SkladoveKarty.Models;
+
+    public static class SettingHelper
+    {
+        public static bool GetBoolValue(IDatabaseContext databaseContext, string name)
+        {
+            var setting = GetSetting(databaseContext, name);
+
+            if (setting == null) return false;
+
+            if (bool.TryParse(setting.Value, out bool result)) return result;
+
+            throw new InvalidOperationException($"'{name}' value isn't boolean.");
+        }
+
+        public static void SaveValue<T>(IDatabaseContext databaseContext, string name, T value)
+        {
+            var setting = GetSetting(databaseContext, name);
+
+            if (setting != null)
+            {
+                setting.Value = value.ToString();
+                return;
+            }
+
+            setting = new()
+            {
+                Name = name,
+                Value = value.ToString(),
+            };
+
+            databaseContext.Settings.Add(setting);
+        }
+
+        private static Setting GetSetting(IDatabaseContext databaseContext, string name)
+        {
+            return databaseContext.Settings.Where(s => s.Name == name).SingleOrDefault();
+        }
+    }
+}
